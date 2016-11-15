83fcddda16598cb13d652bdba89f261f2c1acab2
Yang <Yang@YANG-PC>
2016-11-15 11:33:39 +1100

    checkin

---
+++ Assessment/Controllers/ContactController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Assessment.Models;
+
+namespace Assessment.Controllers
+{
+    public class ContactController : ApiController
+    {
+        private readonly Models.ContactsService _contacServices;
+
+        #region Public Constructor
+
+        /// <summary>
+        /// Public constructor to initialize product service instance
+        /// </summary>
+        public ContactController()
+        {
+            _contacServices = new ContactsService();
+        }
+        #endregion
+
+        public HttpResponseMessage GetAllProducts()
+        {
+            var contacts= _contacServices.GetAll();
+            if (contacts != null)
+            {
+                if (contacts.Count>0)
+                    return Request.CreateResponse(HttpStatusCode.OK, contacts);
+            }
+            return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Products not found");
+        }
+    }
+}
